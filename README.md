f490b354cc70e43cbca805c0e2027b57c0db4477
Chris Pillion <cpillion@techsoft3d.com>
2021-01-20 13:27:55 -0800

    Removing webapck/babel dependency. Adding live-server. Removing import module commands. Updating package.json and README. Clearing class logic for template.

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# Additive Manufacturing Completed
+# Additive Manufacturing Template
 
-This repository contains a completed version of TechSoft 3D's [Additive Manufacturing tutorial](https://docs.techsoft3d.com/communicator/latest/build/tutorials/additive-manufacturing/intro.html) for HOOPS Communicator 2020.
+This repository contains a starting template of TechSoft 3D's [Additive Manufacturing tutorial](https://docs.techsoft3d.com/communicator/latest/build/tutorials/additive-manufacturing/intro.html) for HOOPS Communicator.
 
 See it running live on [github pages](https://techsoft3d.github.io/additive-manufacturing/)
 
@@ -15,5 +15,4 @@ Verify you have the latest version of `npm` installed:
 1. Clone the project: `git clone https://github.com/techsoft3d/additive-manufacturing.git`
 2. Navigate to the project folder: `cd additive_manufacturing_completed`
 3. Install dependencies: `npm install` 
-4. Start the development server: `npm start`
-5. View the project at [http://localhost:8080](http://localhost:8080) 
+4. Start the development server: `npm run start`
--- src/js/syncHelper.js
+++ src/js/syncHelper.js
@@ -1,55 +1,5 @@
-
 class syncHelper {
-    constructor(viewerList) {
-        // Copy the array, so we do not modify the reference values
-        let tfViewerList = viewerList.slice(0);
-        // Assign the first element to the 
-        this._mainViewer = tfViewerList.shift();
-        // All remaining viewers are attached
-        this._attachedViewers = tfViewerList;
-        this._needsUpdate = true;
-        this._modelTreeNodes = [];
-    }
-    syncNodeTransforms(nodeIds = []) {
-        if (this._needsUpdate == true) {
-            this._modelTreeNodes = [];
-            this._gatherAllNodeIds(this._mainViewer.model.getAbsoluteRootNode(), this._modelTreeNodes);
-            this._modelTreeNodes = this._modelTreeNodes.filter(Boolean);
-            this._needsUpdate = false;
-        }
-        nodeIds = nodeIds.length == 0 ? this._modelTreeNodes : nodeIds;
-        let matMap = new Map();
-        for (let node of nodeIds) {
-            matMap.set(node, this._mainViewer.model.getNodeMatrix(node));
-        }
-        for (let [node, matrix] of matMap.entries()) {
-            this._attachedViewers.map((viewer) => {
-                viewer.model.setNodeMatrix(node, matrix);
-            });
-        }
-    }
-    _gatherAllNodeIds(parent, nodeIds) {
-        nodeIds.push(parent);
-        let children = this._mainViewer.model.getNodeChildren(parent);
-        if (children.length !== 0) {
-            for (let child of children) {
-                this._gatherAllNodeIds(child, nodeIds);
-            }
-        }
-    }
-    addInstanceNodeId(nodeId) {
-        this._modelTreeNodes.push(nodeId);
-    }
-    setNeedsUpdate(option) {
-        this._needsUpdate = option;
-    }
-    getModelTreeNodes() {
-        return this._modelTreeNodes;
-    }
-    getMainViewer() {
-        return this._mainViewer;
-    }
-    getAttachedViewers() {
-        return this._attachedViewers;
+    constructor() {
+        
     }
 }
